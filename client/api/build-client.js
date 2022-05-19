e61c705f3aa9f3addad6a3680dbdc5b9ed38b320
Behnam Shayani <behnam.shayani@accenture.com>
2022-05-19 15:33:26 +0200

    small change

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://ticketing.behnamshayani.com',
+      baseURL: 'http://ticketing.behnamshayani.com/',
       headers: req.headers,
     });
   } else {
